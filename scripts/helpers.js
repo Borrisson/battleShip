10972cb44846ecb2a97992c8fb9cb176a5f1c506
Borrisson <lucborris@gmail.com>
2021-01-24 15:20:06 +0000

    Add test case for helper function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,8 +1,10 @@
 const _ = require("lodash");
 
 const shipRange = (firstTile, ship) => {
-	const length = firstTile + ship;
-	if (length > 10) {
+  const length = firstTile + ship;
+  if(ship > 5) {
+    return null;
+  } else if (length > 10) {
 		return null;
 	} else {
 		return _.range(firstTile, length);
--- test/helpers-test.js
+++ test/helpers-test.js
@@ -1,3 +1,22 @@
-const mocha = require('mocha');
-const assert = require('chai');
+const mocha = require("mocha");
+const assert = require("chai").assert;
+const { shipRange } = require("../scripts/helpers");
+const { describe, it } = require("mocha");
 
+describe("shipRange", () => {
+  it("should return null if two arguments sum is greater than 10", () => {
+    const ship = 5;
+    const result = shipRange(6, ship);
+    assert.isNull(result);
+  });
+  it("should return null if ship length is greater than 5", () => {
+    const ship = 6;
+    const result = shipRange(3, ship);
+    assert.isNull(result);
+  });
+  it("should return [2,3,4,5,6] for shipRange(2,5)", () => {
+    const ship = 5;
+    const result = shipRange(2, ship);
+    assert.deepEqual(result, [2,3,4,5,6]);
+  });
+});
